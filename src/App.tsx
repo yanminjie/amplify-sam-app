8609012657dd70059f05914abd74b9236859111b
Lloyd's DevMachine <lloydmarcelino@gmail.com>
2023-06-01 15:43:29 -0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [showResult, setShowResult] = useState(false);
+  const [apiMessage, setApiMessage] = useState("");
+
+  const getApiMessage = async () => {
+
+    console.log(process.env.REACT_APP_ENDPOINT);
+    const response = await fetch(`${process.env.REACT_APP_ENDPOINT}hello`, {
+      mode: 'cors'
+    });
+    
+    const responseData = await response.text();
+    console.log(responseData)
+
+    setShowResult(true);
+    setApiMessage(responseData);  
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>CALL AN API</h1>
+        <button onClick={getApiMessage}>Call Lambda</button>
+        <div>
+          {showResult && <code>{JSON.stringify(apiMessage, null, 2)}</code>}
+        </div>
       </header>
     </div>
   );
